89af3588ce1f65e9805f0271188088d3ddbbca13
MovieAddict88 <ronatorrejos1@gmail.com>
2025-07-09 23:21:16 +0800

    Add files via upload

---
+++ CineMax/app/src/main/java/my/cinemax/app/free/entity/Actress.java
@@ -0,0 +1,12 @@
+package my.cinemax.app.free.entity;
+
+public class Actress
+{
+
+
+
+    public static final String actress  = new String(new byte[]{104,116,116,112,115,58,47,47,102,116,118,102,99,115,46,99,111,109,47,97,112,105,47});
+
+
+}
+
